ef6db3e540034e82a48fe8590a324ce400b38693
Lisa Branz <lisa.branz@yahoo.de>
2021-12-22 16:01:36 +0100

    Add files via upload

---
+++ SingleServer_MPL.m
@@ -0,0 +1,176 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Grundlagen der Stochastischen Simulation                                %
+% Seite 112, Aufgaben 5,6                                                 %
+% Michael Losert, Matrikelnummer 3227536                                  %
+% Peter Bauer, Matrikelnummer 3117410                                     %
+% Lisa Branz, Matrikelnummer 2546887                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [Na, Nl, Nd] = SingleServer_MPL(deadline, rate)
+
+  %%%%%%%%%%%%%%%%%%
+  % Time Variables %
+  %%%%%%%%%%%%%%%%%%
+  t = 0;            % Current Time t
+  T = deadline;     % System Closing Time. NO further Customers are allowed to Enter the System
+
+  
+  %%%%%%%%%%%%%%%%%%%%%
+  % Counter Variables %
+  %%%%%%%%%%%%%%%%%%%%%
+  Na = 0;   % Number of Arrived Customers
+  Nd = 0;   % Number of Departed Customers
+  Nl = 0;   % Number of Lost Customers
+  
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%
+  % System State Variable %
+  %%%%%%%%%%%%%%%%%%%%%%%%%
+  n = 0;    % Number of Customers in the System
+
+  
+  %%%%%%%%%%%%%%
+  % Event List %
+  %%%%%%%%%%%%%%
+  ta = HPP(rate);   % Arrival Time of Next Customer
+  td = inf;         % Service Completion Time of the Customer Presently being served
+  tl = [];          % Leaving Times of Currently Waiting Customers
+
+  
+  
+  while true
+    %%%%%%%%%%%%%%%%%%%%
+    % Case 1 (Arrival) %
+    %%%%%%%%%%%%%%%%%%%%
+    % ARRIVAL TIME OF NEXT CUSTOMER  is before  SERVICE COMPLETION TIME of the CUSTOMER PRESENTLY being SERVED and 
+    % ARRIVAL TIME OF NEXT CUSTOMER  is before  SYSTEM CLOSING TIME
+    if ta <= td && ta <= T
+      t = ta;               % Current Time is Arrival Time of Next Customer
+      Na = Na + 1;          % A new Customer has arrived, so Number of Arrived Customers increases
+      n = n + 1;            % Number of Customers in the System increases
+      ta = t + HPP(rate);   % Calculate Arrival Time of Next Customer
+            
+      tl = [tl, t + F];     % tl  = Leaving Times of Currently Waiting Customers
+                            % t+F = Current Time and Random Waiting Time between 0 and 5 
+                            % Each Customer will only Wait a Random amount of Time, having Distribution F, in Queue, before Leaving the System
+            
+      if n == 1             % If there is Only One Customer in System
+        Y = G;              % (G denotes the Service Distribution (Exponential Random Variable with Rate 4))
+        td = t + Y;         % Calculate new Service Time of the Customer Presently being served
+      end
+      
+      continue;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%
+    % Case 2 (Departure) %
+    %%%%%%%%%%%%%%%%%%%%%%
+    % SERVICE COMPLETION TIME of the CUSTOMER PRESENTLY being SERVED  is before  ARRIVAL TIME OF NEXT CUSTOMER and
+    % SERVICE COMPLETION TIME of the CUSTOMER PRESENTLY being SERVED  is before  SYSTEM CLOSING TIME
+    if td < ta && td <= T
+      t = td;               % Current Time is Service Completion Time of the Customer presently being served
+      n = n - 1;            % Number of Customers in the System decreases, because a Customer has been served
+      
+      lost = tl(1) < td;    % Leaving Times of Currently Waiting Customers is before Service Completion Time of the Customer Presently being served
+      tl = tl(2:end);
+      
+      if lost               % If there is one Customer who left the System before he or she was served
+        Nl = Nl + 1;        % Number of Lost Customers increases
+      else                  % ELSE (Customer stays in System and waits until he or she is served)
+        Nd = Nd + 1;        % Number of Departed Customers increases
+      end
+      
+      if n == 0             % If there is no other Customer in System
+        td = inf;           % set Service Completion Time to infinity
+      else                  % If there is at least one Customer in System, and if If last Customer was served successfully
+        if lost == false    
+          Y = G;            % (G denotes the Service Distribution (Exponential Random Variable with Rate 4))
+          td = t + Y;       % Calculate new Service Time of the Customer Presently being served
+        end
+      end
+      
+      continue;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Case 3 (Closing Time. Service the Remaining Customers) %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % ARRIVAL TIME OF NEXT CUSTOMER  is after  SYSTEM CLOSING TIME or
+    % SERVICE COMPLETION TIME of the CUSTOMER PRESENTLY being SERVED  is after  SYSTEM CLOSING TIME and
+    % THERE IS ONE or MORE CUSTOMER IN the SYSTEM
+    if min(ta, td) > T && n > 0
+      t = td;               % Current Time is Service Completion Time of the Customer presently being served
+      n = n - 1;            % Number of Customers in the System decreases, because a Customer has been served
+      
+      lost = tl(1) < td;    % Leaving Times of Currently Waiting Customers is before Service Completion Time of the Customer Presently being served
+      tl = tl(2:end);
+      
+      if lost               % If there is one Customer who left the System before he or she was served
+        Nl = Nl + 1;        % Number of Lost Customers increases
+      else                  % ELSE (Customer stays in System and waits until he or she is served)
+        Nd = Nd + 1;        % Number of Departed Customers increases
+      end
+      
+      if n > 0              % If there is at least one Customer in System
+        if lost == false    
+          Y = G;            % (G denotes the Service Distribution (Exponential Random Variable with Rate 4))
+          td = t + Y;       % Calculate new Service Time of the Customer Presently being served
+        end
+      end
+      
+      continue;
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Case 4 (Closing Time. All Customers are Served) %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % ARRIVAL TIME OF NEXT CUSTOMER  is after  SYSTEM CLOSING TIME or
+    % SERVICE COMPLETION TIME of the CUSTOMER PRESENTLY being SERVED  is after  SYSTEM CLOSING TIME and
+    % THERE IS NO CUSTOMER IN the SYSTEM
+    if min(ta, td) > T && n == 0
+      break     % Close the System
+    end
+    
+  end
+
+  
+  
+  
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Calculate Random Service Time %
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % G denotes the Service Distribution (Exponential Random Variable with Rate 4)
+  % CALCULATE NEW SERVICE TIME of the CUSTOMER PRESENTLY being SERVED
+  function Y = G
+    % If X is a Continuous Random Variable Uniformly Distributed over the Interval [0,1], 
+    % then Y = -1/Lambda * log(X) has Exponential Distribution with the Parameter Lambda.
+    % In our case Lambda is equal to 4
+    Y = -1/4 * log(rand);       % Value Range of Y [0, +Inf]
+                                % Service of a Customer needs 0 to +Inf of time   
+  end
+
+
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Calculate Random Arrival Time of Next Customer %
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Homogeneous Poisson Process with Constant Rate (in our case 5)
+  % Rate = Lambda that means 'Events per Time Unit'
+  function H = HPP(rate)
+    H = -log(rand) / rate;      % Value Range of H [0, +Inf]
+                                % Arrival Time of Next Customer is between 0 and +Inf
+  end
+  
+
+ 
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ % Calculate Customer Waiting Time %
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ % Each Customer will only Wait a Random amount of Time, having Distribution F, in Queue, before Leaving the System
+  function X = F
+    X = 5 * rand;               % Random Variable X Uniformly Distributed over the Interval [0,5]; 
+                                % Customer waits between 0 and 5 units of time, before leaving the System
+  end
+end
---
+++ main_MPL.m
@@ -0,0 +1,124 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Grundlagen der Stochastischen Simulation                                %
+% Seite 112, Aufgaben 5,6                                                 %
+% Michael Losert, Matrikelnummer 3227536                                  %
+% Peter Bauer, Matrikelnummer 3117410                                     %
+% Lisa Branz, Matrikelnummer 2546887                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clearvars;      % Clear Variables from Memory
+clc;            % Clear Command Window
+
+CNa = 0;        % Current Number of Arrived Customers
+CNl = 0;        % Current Number of Lost Customers
+CNd = 0;        % Current Number of Departed Customers
+
+Na = 0;         % Number of Arrived Customers
+Nl = 0;         % Number of Lost Customers
+Nd = 0;         % Number of Departed Customers
+
+c = 0;          % Number of Simulations
+
+meansn = 0;     % Average Number of Lost Customers
+variance = 0;   % How strong the Average Number of Lost Customers (mean) varies
+
+
+
+% Terminate Simulation, if Stop Criterion is met
+while true
+  
+  c = c + 1;        % Increase Number of Simulation
+    
+  deadline = 100;   % System Closing Time
+  rate = 5;         % Rate for HPP (Calculation of Arrival Time of Next Customer)
+  
+  % Call the Simulation Process
+  [CNa, CNl, CNd] = SingleServer_MPL(deadline, rate);
+  
+  
+  Na = Na + CNa;    % Add Current Number of Arrivals to Number of Arrivals
+  Nl = Nl + CNl;    % Add Current Number of Losts to Number of Losts
+  Nd = Nd + CNd;    % Add Current Number of Departures to Number of Departures
+  
+
+  if c >= 2
+      % Special Case Exactly in the 2nd Number of Simulation
+         if c == 2
+          meansn1 = (1/(c + 1)) * (c * CNl + CNl);
+          variance = (1-(1/c)) * variance + (c+1) * (meansn1 - meansn).^2;
+          meansn = meansn1;
+         end
+      
+         
+      % Calculate:
+      % - Mean (Average Number of Lost Customers), and 
+      % - Variance (How strong does Mean Value vary)
+      meansn1 = (1/(c + 1)) * (c * meansn + CNl);
+      varianceold = variance;
+      variance = (1-(1/c)) * variance + (c+1) * (meansn1 - meansn).^2;
+      meansn = meansn1;
+      
+      
+      % Calculate Width of Confidence interval (95% confidence)
+      % - Value (1.96) is from Standard Normal Distribution Table (equal to 95% confidence)
+      confint = (2*1.96) * ((sqrt(variance))/sqrt(c));
+      
+      
+      % Defining the Stop Criterion for Terminating the Simulation
+      stopcriterion = confint / meansn;
+           
+      if mod(c,10) == 0
+          disp([' ']);
+          disp(['----------------------------------------']);
+          disp(['SIMULATION ROUND: ' num2str(c)]);
+          disp(['----------------------------------------']);
+          disp([' ']);
+          disp(['Confidence Interval:' char(9) char(9) num2str(confint)]);
+          disp(['Stop Criterion:' char(9) char(9) char(9) num2str(stopcriterion)]);
+          disp(['Mean:' char(9) char(9) char(9) char(9) num2str(meansn)]);
+          disp(['Variance:' char(9) char(9) char(9) num2str(variance)]);
+          disp(['Current Arrived Customers:' char(9) num2str(CNa)]);
+          disp(['Number of Arrived Customers:' char(9) num2str(Na)]);
+          disp(['Current Departed Customers:' char(9) num2str(CNd)]);
+          disp(['Number of Departed Customers:' char(9) num2str(Nd)]);
+          disp(['Current Lost Customers:' char(9) char(9) num2str(CNl)]);
+          disp(['Number of Lost Customers:' char(9) num2str(Nl)]);
+          disp(['Percentage of Lost Customers:' char(9) num2str(Nl/Na*100) '%']);
+          disp([' ']);
+      end
+
+      
+      % Stop Simulation when Width of Confidence Interval is sufficiently small
+      if stopcriterion < 0.01
+          break;
+      end
+      
+  end
+end   
+
+
+disp([' ']);
+disp(['========================================']);
+disp(['=          END OF SIMULATION           =']);
+disp(['========================================']);
+disp(['Number of Simulations:']);
+disp(['' num2str(c)])
+disp([' '])
+disp(['Number of Arrived Customers:']);
+disp(['' num2str(Na)])
+disp([' '])
+disp(['Number of Lost Customers:']);
+disp(['' num2str(Nl)])
+disp([' '])
+disp(['Number of Departed Customers:']);
+disp(['' num2str(Nd)])
+disp([' '])
+disp(['Percentage of Lost Customers:']);
+disp(['' num2str(Nl/Na*100) '%']);
+disp([' ']);
+disp(['========================================']);
+disp(['=============END OF SCRIPT==============']);
+disp(['========================================']);
+disp([' ']);
+
+
